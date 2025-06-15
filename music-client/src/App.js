33c5796587b5959a70a30980f103ad996b39f24f
Victor Bundi <victorbundi.dev@gmail.com>
2025-06-15 16:51:50 +0300

    First commit

---
+++ music-client/src/App.js
@@ -0,0 +1,14 @@
+import './index.css';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='bg-red-500 text-white text-3xl p-4'>
+        <h3 className=''>Wowwwwwww</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
