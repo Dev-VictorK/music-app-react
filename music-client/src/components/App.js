16a87feae431a3585d488ae7436fd1346815edb7
Victor Bundi <victorbundi.dev@gmail.com>
2025-06-16 22:22:57 +0300

    Tailwind works, Added a bunch of component UI

---
+++ music-client/src/components/App.js
@@ -0,0 +1,18 @@
+import '../index.css';
+import React from 'react';
+import TopBar from './TopBar';
+import AlbumsContainer from './AlbumsContainer';
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div className='grid grid-rows-[auto_1fr] h-screen'>
+      <TopBar />
+      <Routes>
+        <Route path='/albums' element={<AlbumsContainer />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
