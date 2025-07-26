09f2a6f84b5480dd013f3a3d0221f48f7608b97d
Victor Bundi <victorbundi.dev@gmail.com>
2025-07-26 17:11:03 +0300

    sdr

--- music-client/netlify/functions/albums.js
+++ music-client/netlify/functions/albums.js
@@ -1,4 +1,4 @@
-import SpotifyClient from './SpotifyClient.mjs';
+import SpotifyClient from './SpotifyClient.js';
 
 const API_TOKEN = 'BQB03nT4j76U220FuxUY-Haq-qHWKireW6OvirFNdFLk5fI5U-is9U0Wbc9xvvuGHpBJzP9IwJb2TKsffl2lU918g5SjaEjbJAy3gPqvAn1dvjBivYMx_BHHFmvKcE7c-HgruvpoVeA';
 
